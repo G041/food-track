16c8f11a5d98dbcb8cb1f328d0f8755980db5674
Juan-Pedro-Garrahan <juanpgarrahan04@gmail.com>
2025-11-12 04:50:31 -0300

    cambios de estilo en la barra de pestañas para mejorar la apariencia en iOS.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,50 @@
 import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={{
-      headerShown: false,
-    }}>
-      <Tabs.Screen 
-      name="index" 
-      options={{ 
-        title: "Mapa", 
-        tabBarIcon: ({ color, size }) => (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#400101",
+          borderTopColor: "#D9601A",
+          height: Platform.OS === "ios" ? 70 : 65,
+          paddingBottom: Platform.OS === "ios" ? 25 : 10,
+          paddingTop: 10,
+        },
+        tabBarActiveTintColor: "#F2A413",
+        tabBarInactiveTintColor: "#F28E13",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Mapa",
+          tabBarIcon: ({ color, size }) => (
             <FontAwesome5 name="map-marked-alt" size={size} color={color} />
           ),
         }}
       />
-      <Tabs.Screen 
-      name="camera" 
-      options={{ 
-        title: "Escanear un QR", 
-        tabBarIcon: ({ color, size }) => (
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: "Escanear QR",
+          tabBarIcon: ({ color, size }) => (
             <FontAwesome5 name="qrcode" size={size} color={color} />
           ),
         }}
       />
-      <Tabs.Screen 
-      name="home" 
-      options={{ 
-        title: "Home", 
-        tabBarIcon: ({ color, size }) => (
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Inicio",
+          tabBarIcon: ({ color, size }) => (
             <FontAwesome name="home" size={size} color={color} />
           ),
         }}
@@ -37,7 +53,8 @@ export default function RootLayout() {
   );
 }
 
-/*<Stack>
-        <Stack.Screen name="index"/>
-      </Stack>
+/* 
+<Stack>
+  <Stack.Screen name="index" />
+</Stack>
 */
