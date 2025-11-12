227078d494fd93af34eab10b83f2f3651dc3dbaa
Manuel <89187265+ManuGTX@users.noreply.github.com>
2025-11-12 08:53:10 -0300

    Actually remove config.ts from repo

--- utils/config.ts
+++
@@ -1,4 +0,0 @@
-export const API_HOST = "192.168.1.11"; //cambiar por la ip y puerto de TU servidor express
-export const API_PORT = 3000;
-export const API_URL = `http://${API_HOST}:${API_PORT}`;
-//export const API_URL = `https://established-auto-taxes-physical.trycloudflare.com`;
